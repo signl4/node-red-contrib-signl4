2a137c3925cf9645af107f338366a4549567863e
Ron <43244104+rons4@users.noreply.github.com>
2021-01-11 11:35:46 +0100

    Add files via upload

--- signl4.js
+++ signl4.js
@@ -1,4 +1,73 @@
 // MIT License
+// Copyright (c) 2021 Derdack GmbH
+
+module.exports = function(RED) {
+    function SIGNL4(config) {
+		RED.nodes.createNode(this,config);
+		var node = this;
+		node.on('input', function(msg) {
+
+		// Create or change values if required
+		if (msg.payload == undefined) {
+			msg.payload = {
+				'Subject': '',
+				'Body': '',
+				'X-S4-Service': '',
+				'X-S4-Location': '',
+				'X-S4-AlertingScenario': '',
+				'X-S4-ExternalID': ''
+			};
+		}
+		if (msg.payload.Subject == undefined) {
+			msg.payload.Subject = '';
+		}
+		if (msg.payload.Subject == '') {
+			msg.payload.Subject = config.alertSubject;
+		}
+		if (msg.payload.Body == undefined) {
+			msg.payload.Body = '';
+		}
+		if (msg.payload.Body == '') {
+			msg.payload.Body = config.alertBody;
+		}
+		if (msg.payload['X-S4-Service'] == undefined) {
+			msg.payload['X-S4-Service'] = '';
+		}
+		if (msg.payload['X-S4-Service'] == '') {
+			msg.payload['X-S4-Service'] = config.alertS4Service;
+		}
+		if (msg.payload['X-S4-Location'] == undefined) {
+			msg.payload['X-S4-Location'] = '';
+		}
+		if (msg.payload['X-S4-Location'] == '') {
+			msg.payload['X-S4-Location'] = config.alertS4Location;
+		}
+		if (msg.payload['X-S4-AlertingScenario'] == undefined) {
+			msg.payload['X-S4-AlertingScenario'] = '';
+		}
+		if (msg.payload['X-S4-AlertingScenario'] == '') {
+			msg.payload['X-S4-AlertingScenario'] = config.alertS4AlertingScenario;
+		}
+		if (msg.payload['X-S4-ExternalID'] == undefined) {
+			msg.payload['X-S4-ExternalID'] = '';
+		}
+		if (msg.payload['X-S4-ExternalID'] == '') {
+			msg.payload['X-S4-ExternalID'] = config.alertS4ExternalID;
+		}
+
+		// Send alert
+		// msg.payload = msg.payload.toLowerCase();
+		sendSIGNL4Alert(node, config.teamSecret, msg.payload);
+
+		if (node.done) {
+			node.done();
+		}
+		
+		node.send({payload:'OK', statusCodenumber: 200});
+
+        });
+    }
+    RED.nodes.// MIT License
 // Copyright (c) 2020 Derdack GmbH
 
 
@@ -120,13 +189,9 @@ module.exports = function(RED) {
 
         });
 	}
-
-
-    RED.nodes.registerType("SIGNL4", SIGNL4);
-	
 	RED.nodes.registerType("SIGNL4 Resolve", SIGNL4Resolve);
 
-	function sendSIGNL4Alert(node, teamsecret, body) {
+	function SIGNL4Alert(node, teamsecret, body) {
 		
 		try {
 
@@ -158,7 +223,7 @@ module.exports = function(RED) {
 					} else {
 						// Error
 						node.log('SIGNL4 alert failed.');
-						var msg = { payload: { error: 'SIGNL4 alert failed. Status code: ' + res.statusCode }};
+						var msg = { payload: { error: 'SIGNL4 alert failed.' }};
 						node.error(msg);
 					} 
 				});
@@ -182,4 +247,5 @@ module.exports = function(RED) {
 		}
 
     }
+	RED.nodes.registerType("SIGNL4", SIGNL4);
 }
